09eb8c177d569055aba7ebc36c0b296d82a19e6d
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-02-07 17:28:01 +0200

    20170207

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-var vasya = {name: "Vasya",
+var wrapper = document.createElement('div');
+var headerText = document.createElement('h1');
+
+document.createElement('ul');
+for (var i = 0; i < 3; i++) {
+  document.createElement('li');
+}
+
+
 
 greetings: function() {
 
