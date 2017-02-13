327ab46d76922f1c4b14d5a52681d8d37c2da06e
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-02-13 16:00:33 +0200

    some renames

--- js/script.js
+++ js/script.js
@@ -11,11 +11,11 @@ var qaItem = [];
 
 var checkbox = [];
 
-var button = document.createElement('button');
-button.type = "submit";
-button.className = "pure-button pure-button-primary";
-button.innerHTML = "Проверить мои результаты";
-button.style.margin = "50px 0 0 0";
+var submitButton = document.createElement('button');
+submitButton.type = "submit";
+submitButton.className = "pure-button pure-button-primary";
+submitButton.innerHTML = "Проверить мои результаты";
+submitButton.style.margin = "50px 0 0 0";
 
 function checklist(mainBlock) {
   for (var i = 1; i <= 3; i++) {
@@ -44,4 +44,4 @@ for (var j = 1; j <= 3; j++) {
   wrapper.appendChild(qaItem[j]);
   checklist(wrapper);
 }
-wrapper.appendChild(button);
+wrapper.appendChild(submitButton);
